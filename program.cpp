e24a9109f85be56bcbf7552aaeb276c23ee47f28
kholcomb914 <k1t.holcomb@outlook.com>
2024-10-22 09:40:31 -0500

    added displayBoard method

--- program.cpp
+++ program.cpp
@@ -14,3 +14,9 @@ bool isValidMove(const std::vector<std::vector<char>>& board, int x, int y) {
     
     return true;
 }
+
+void displayBoard(char[3][3] board) {
+	std::cout << board[0][0] << " " << board[0][1] << " " << board[0][2] << std::endl;
+	std::cout << board[1][0] << " " << board[1][1] << " " << board[1][2] << std::endl;
+	std::cout << board[2][0] << " " << board[2][1] << " " << board[2][2] << std::endl;
+}
